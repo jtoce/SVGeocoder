17353b6a8a15f3cc4069abe27d23cba7f253f09c
Jeffrey Toce <jeff@funktional.com>
2013-01-17 14:29:29 -0800

    Added target to SVGeocode: * SVGeocoderLib is a framework library for use in other projects

---
+++ Demo/SVGeocoderLib/SVGeocoderLib.h
@@ -0,0 +1,13 @@
+//
+//  SVGeocoderLib.h
+//  SVGeocoderLib
+//
+//  Created by Jeffrey Toce on 7/29/12.
+//  Copyright (c) 2012 Slingshop, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SVGeocoderLib : NSObject
+
+@end
---
+++ Demo/SVGeocoderLib/SVGeocoderLib.m
@@ -0,0 +1,13 @@
+//
+//  SVGeocoderLib.m
+//  SVGeocoderLib
+//
+//  Created by Jeffrey Toce on 7/29/12.
+//  Copyright (c) 2012 Slingshop, Inc. All rights reserved.
+//
+
+#import "SVGeocoderLib.h"
+
+@implementation SVGeocoderLib
+
+@end
